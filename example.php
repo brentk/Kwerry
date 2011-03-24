70bb2004089aea2db4b51a1e34ed118982d90647
Brent Kelly <brenttkelly@gmail.com>
2011-03-23 23:44:39 -0500

    Added a few more tests

--- example.php
+++ example.php
@@ -12,12 +12,19 @@ $kwerry_opts[ "default" ][ "port" ]	= "5432";
 $kwerry_opts[ "default" ][ "dbname" ]	= "bkellydb";
 $kwerry_opts[ "default" ][ "username" ]	= "brentkelly";
 $kwerry_opts[ "default" ][ "password" ]	= "p@\$\$w0rd";
+
+
+$kwerry_opts[ "testdb" ][ "dbtype" ]	= "postgresql";
+$kwerry_opts[ "testdb" ][ "host" ]	= "localhost";
+$kwerry_opts[ "testdb" ][ "port" ]	= "5432";
+$kwerry_opts[ "testdb" ][ "dbname" ]	= "testdb";
+$kwerry_opts[ "testdb" ][ "username" ]	= "testuser";
+$kwerry_opts[ "testdb" ][ "password" ]	= "testuser";
 //$kwerry_opts[ "default" ][ "prefix" ]	= "tbl_";
 //$kwerry_opts[ "default" ][ "suffix" ]	= "";
 
 try{
 
-	/*
 	$writer = Kwerry::model( "tbl_writer" );
 	$news = $writer->whereName( "Brent" )->tbl_news->sortInsertstamp();
 
@@ -28,17 +35,26 @@ try{
 			echo $comment->date."\n";
 		}
 	}
-	*/
-
-	
 
 	$test = Kwerry::model( "tbl_test" );
 	$test->whereTest_ID( 2 );
 	echo $test->testname."\n";
-	$test->testname = "testtwo";
+
+	$value = "random_value_".rand( 100,999 );
+	echo "Setting value to $value\n";
+	$test->testname = $value;
 	$test->update();
 	echo $test->testname."\n";
+
+	$othertest = Kwerry::model( "tbl_test", "testdb" );
+	echo $othertest->columnone."\n";
+	$value = "random_value_".rand( 100,999 );
+	echo "Setting value to $value\n";
+	$othertest->columnone = $value;
+	$othertest->update();
+	echo $othertest->columnone."\n";
 	
+
 
 } catch( Exception $e ) {
 	echo "\n********* ERROR ***********\n";
